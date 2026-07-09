a786de5a1a6c55ba2e7fdf5b5b367b6cb1a83e63
orssdev <orss.dev@gmail.com>
2026-07-08 22:38:09 -0400

    (nvim): add toggleterm.nvim for floating terminal toggle

---
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        lazy = true,
+        cmd = { 'ToggleTerm' },
+        keys = {
+            { '<leader>t', '<cmd>ToggleTerm direction=float<cr>', desc = 'Toggle floating terminal' },
+        },
+        opts = {
+            direction = 'float',
+        },
+    },
+}
