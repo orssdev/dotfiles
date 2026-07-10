16b4c03975374c4d703a6d2b1402fecfc53b7945
orssdev <orss.dev@gmail.com>
2026-07-10 00:15:32 -0400

    (nvim): add autoread with checktime autocmd for external file reload

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -9,6 +9,15 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
+vim.opt.autoread = true
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})
 
 vim.diagnostic.config({
   float = { border = "rounded" },
