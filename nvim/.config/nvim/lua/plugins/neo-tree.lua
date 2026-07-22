9be374901716fc9f2c290fb0cc35dfb4283d393e
orssdev <orss.dev@gmail.com>
2026-07-22 03:04:35 -0400

    (nvim): add neo-tree source selector and git status symbols

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -11,6 +11,31 @@ return {
       { '<leader>et', '<cmd>Neotree toggle<cr>', desc = 'Toggle NeoTree' },
     },
     opts = {
+      source_selector = {
+        winbar = true,
+        statusline = false,
+        content_layout = 'center',
+        sources = {
+          { source = 'filesystem', display_name = '󰉓 Files' },
+          { source = 'buffers', display_name = '󰈙 Bufs' },
+          { source = 'git_status', display_name = ' Git' },
+        },
+      },
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            added = '✚',
+            modified = '',
+            deleted = '✖',
+            renamed = '',
+            untracked = 'U',
+            ignored = 'I',
+            unstaged = 'M',
+            staged = 'S',
+            conflict = 'C',
+          },
+        },
+      },
       filesystem = {
         follow_current_file = { enabled = true },
         filtered_items = {
