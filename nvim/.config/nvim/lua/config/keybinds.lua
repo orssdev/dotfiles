22b9dc8196adeaee8e8b9274144c20d687b39c80
orssdev <orss.dev@gmail.com>
2026-07-23 03:30:20 -0400

    (nvim): remove leader x keymap

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>x', vim.cmd.x)
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { silent = true })
 vim.keymap.set('n', '<leader>d', function() vim.diagnostic.open_float({ focus = true }) end)
