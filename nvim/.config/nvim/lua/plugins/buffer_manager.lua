f0852d223e12a2ca85bba8db81e92bb55de91f7e
orssdev <orss.dev@gmail.com>
2026-07-23 02:16:16 -0400

    (nvim): add buffer_manager plugin

---
+++ nvim/.config/nvim/lua/plugins/buffer_manager.lua
@@ -0,0 +1,9 @@
+return {
+  'j-morano/buffer_manager.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('buffer_manager').setup()
+    local ui = require('buffer_manager.ui')
+    vim.keymap.set('n', '<leader>m', ui.toggle_quick_menu, { desc = 'Buffer manager' })
+  end,
+}
