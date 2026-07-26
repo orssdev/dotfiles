423ff8574215f678f7b140859f487a62ac32aadc
orssdev <orss.dev@gmail.com>
2026-07-26 18:22:10 -0400

    feat(nvim): toggle terminal from any mode with ctrl+backslash

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -5,10 +5,11 @@ return {
     lazy = true,
     cmd = { 'ToggleTerm' },
     keys = {
-      { '<leader>t', '<cmd>ToggleTerm direction=float<cr>', desc = 'Toggle floating terminal' },
+      { [[<c-\>]], '<cmd>ToggleTerm direction=float<cr>', desc = 'Toggle floating terminal' },
     },
     opts = {
       direction = 'float',
+      open_mapping = [[<c-\>]],
     },
   },
 }
