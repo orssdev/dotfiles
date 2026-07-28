cef99fa4ea555ccc1af8e5be32823bec29616276
orssdev <orss.dev@gmail.com>
2026-07-28 01:18:50 -0400

    chore(zsh): move machine-specific paths to gitignored local overrides

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -6,10 +6,3 @@ export VISUAL="nvim"
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
-
-# Android
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
